4943781d5e672a3ef4934c4032c6a34c6f8093e3
Lungelo Mbese <Lungelo.Mbese@standardbank.co.za>
2024-05-15 18:53:18 +0200

    Adds urls docstring

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
+""" URL patterns for webapp. """
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
